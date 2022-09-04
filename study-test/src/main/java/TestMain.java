dcfb38b6d41b247c574c2690d5ea29240ef8df25
makai <kai.ma@2339.com>
2022-09-04 09:55:43 +0800

    feat-保存代码

--- study-test/src/main/java/TestMain.java
+++ study-test/src/main/java/TestMain.java
@@ -1,10 +1,11 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestMain {
 	public static void main(String[] args) {
-		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-		User user = (User) context.getBean("user");
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		User user = (User) applicationContext.getBean("user");
 		System.out.println(user);
 	}
 }
